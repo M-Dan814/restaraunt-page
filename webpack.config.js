437ed7a48e061b44374ceb21e2c9e375ebedf00e
Dayan <m.dayan5689@gmail.com>
2022-11-09 15:39:25 -0800

    Added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
     {
       test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
       type: "asset/resource",
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
     }
     ],
   },
